142c3df1d62d5d7e3fe24cd438d6f4ab4fd4336b
mehdi <parastar.mehdi@gmail.com>
2019-07-05 01:41:06 -0500

    const port added

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 const express = require('express');
-
+const port = 3000;
 const app = express();
 
 app.get('/', (req, res) => {
     res.send('Hello');
 });
 
-app.listen(3000, () => {
-    console.log('server is running on port 3000');
+app.listen(port, () => {
+    console.log(`server is running on port ${port}`);
 });
